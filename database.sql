cb62ff64280629c97e6f7f65793c75434ff1ee57
Asif Ahmad <asifahmad280820ttp@gmail.com>
2026-08-17 00:17:37 +0600

    Delete database.sql

--- database.sql
+++
@@ -1,3 +0,0 @@
-CREATE DATABASE IF NOT EXISTS azaro_fashion CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-USE azaro_fashion;
--- Run setup.php for the complete AZARO schema and demo fashion catalog.
